da0b0e6b05a7b3d9d008894ad58aff60daf7479f
Fabio <fabio.7.45@hotmail.com>
2023-10-30 11:11:53 -0300

    aprendendo sobre tipos imutaveis

--- .vscode/Tiposimutaveis.py
+++ .vscode/Tiposimutaveis.py
@@ -3,5 +3,11 @@
 https://docs.python.org/pt-br/3/library/stdtypes.html
 Imutáveis que vimos: str, int, float, bool
 """
-string  = 'Jose Fabio'
-
+string  = 'jose Fabio'
+outra_variavel = {f'{string[:3]}ABC{string[4:]}'}
+print(string)
+print(outra_variavel) # strings sao imutaveis
+print(string.capitalize())
+print(string.upper())
+print(string.lower())
+print(string.zfill(100))# vai aparecer  zeros a esquerda do sua string
