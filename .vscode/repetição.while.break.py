9d389430d286664c92700bbd354d841980988c95
Fabio <fabio.7.45@hotmail.com>
2023-10-31 11:34:39 -0300

    continue

--- .vscode/repetição.while.break.py
+++ .vscode/repetição.while.break.py
@@ -5,12 +5,43 @@ Executa uma ação enquanto uma condição for verdadeira
 Loop infinito -> Quando um código não tem fim
 """
 
-condicao = True
+'''condicao = True
 while condicao:
   nome = input('Digite seu nome:')
   print(f'seu nome é {nome}')
   if nome == 'sair':
     break
-print('acabou')
+print('acabou')'''
      
-    
+'''contador = 0
+while contador <20:
+  contador = contador +1
+  print(contador)
+  
+print('Acabou!')'''
+
+# Operadores de atribuição
+# = += -= *= /= //= **= %=
+
+#contador = 0
+
+'''while contador < 100:
+  contador += 1
+  
+  if contador >= 40 and contador <= 70:
+    print('não vou mostrar o',contador)
+    continue
+  print(contador)
+  
+  if contador == 90:
+    break
+print('Acabou!')'''
+  
+contador = 0
+ 
+while contador < 100:
+  contador += 1
+  print(contador)
+  continue
+  if contador == 60:
+    print(contador)
