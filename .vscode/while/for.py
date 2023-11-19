0e356c0180341fc4121495920931561c94972fdb
Fabio <fabio.7.45@hotmail.com>
2023-11-18 22:44:52 -0300

    for

--- .vscode/while/for.py
+++ .vscode/while/for.py
@@ -1,23 +1,37 @@
-# texto = 'python'
+# # texto = 'python'
 
-# i = 0
+# # i = 0
 
-# Tm_string = len(texto) 
-# while i < Tm_string:
+# # Tm_string = len(texto) 
+# # while i < Tm_string:
    
-#     print(texto[i],i)
-#     i += 1
+# #     print(texto[i],i)
+# #     i += 1
 
-senha = '1234567'
-senhaDigitada = ''
-repeticoes = 0 
+# senha = '1234567'
+# senhaDigitada = ''
+# repeticoes = 0 
 
-while senha != senhaDigitada:
-    senhaDigitada = input(f'sua senha({repeticoes}):')
-    repeticoes += 1
+# while senha != senhaDigitada:
+#     senhaDigitada = input(f'sua senha({repeticoes}):')
+#     repeticoes += 1
     
-print(repeticoes)
-print('Aquele laço acima pode ter repetições infinitas.')
+# print(repeticoes)
+# print('Aquele laço acima pode ter repetições infinitas.')
 
 
+#texto = 'JoseFabio'
+# novoTexto = ''
+# for letra in texto: # variavel letra e criada dentro do for.
+#     novoTexto += f'*{letra}'
+#     print(letra)
+# print(novoTexto,f'*')  
 
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+numeros = range(0, 100, 8)
+
+for numero in numeros:
+    print(numero)
